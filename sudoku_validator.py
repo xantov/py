2d9b39aa6478559542d8ad77713c037123abff52
xantov <34904222+xantov@users.noreply.github.com>
2020-06-13 18:18:36 +0700

    Update sudoku_validator.py

--- sudoku_validator.py
+++ sudoku_validator.py
@@ -4,16 +4,16 @@ Sudoku = True
 # create board, read lines, columns and sub boxes
 board = [[i for i in x] for x in input.split()]
 if sum([len(x) for x in board]) != 81:
-    all = []
+    all_sudoku = []
     Sudoku = False
 else:
     line = [[board[y][x] for x in range(9)] for y in range(9)]
     column = [[board[y][x] for y in range(9)] for x in range(9)]
     sub_box = [[board[y0 * 3 + y][x0 * 3 + x] for x in range(3) for y in range(3)] for x0 in range(3) for y0 in range(3)]
-    all = line + column + sub_box
+    all_sudoku = line + column + sub_box
 
 # Check the sudoku
-for el in all:
+for el in all_sudoku:
     tmp = [int(i) for i in el if i.isdigit() and 0 < int(i) <=9]
     Sudoku = False if not tmp else True
     if len(tmp) != 9 or sum(tmp) != 45:
